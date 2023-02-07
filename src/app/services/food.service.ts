4cef866b22e623ec4c1afa6a58dbdf964d8fde42
Afolabi <opakunleafolabi@gmail.com>
2023-02-07 20:17:40 +0100

    sample foods returned

---
+++ src/app/services/food.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Food } from '../shared/models/food';
+import { HttpClient } from '@angular/common/http';
+import { sample_foods } from '../../data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Food[] {
+    return sample_foods;
+  }
+
+}
