e519e75ccb7ec5a15323356de1074104c50a6d4e
Afolabi <opakunleafolabi@gmail.com>
2023-02-07 22:05:41 +0100

    router outlet changed

--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -14,4 +14,8 @@ export class FoodService {
     return sample_foods;
   }
 
+  getAllFoodsBySearchTerm(searchTerm: string) {
+    return this.getAll().filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()))
+  }
+
 }
