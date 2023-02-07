980d1ccb7cdd75f23100b3116a8020e6eba8036a
Afolabi <opakunleafolabi@gmail.com>
2023-02-07 22:21:58 +0100

    get food by search url implemented

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Food } from '../../../shared/models/food';
 import { FoodService } from '../../../services/food.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,11 +11,19 @@ import { FoodService } from '../../../services/food.service';
 export class HomeComponent implements OnInit {
   foods: Food[] = [];
 
-  constructor(private foodService: FoodService) {
+  constructor(private foodService: FoodService,
+              private activatedRoute: ActivatedRoute,
+              ) {
   }
 
   ngOnInit() {
-    this.foods = this.foodService.getAll();
+    this.activatedRoute.params.subscribe(params => {
+      if(params['searchTerm']) {
+        this.foods = this.foodService.getAllFoodsBySearchTerm(params['searchTerm']);
+      } else {
+        this.foods = this.foodService.getAll();
+      }
+    })
   }
 
 }
