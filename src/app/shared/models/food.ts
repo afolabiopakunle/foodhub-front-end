2f180f6a2e96f5bd698f5900df5841cf05b11358
Afolabi <opakunleafolabi@gmail.com>
2023-02-07 19:35:52 +0100

    food class implemented

---
+++ src/app/shared/models/food.ts
@@ -0,0 +1,11 @@
+export class Food {
+  id!: string;
+  name!: string;
+  price!: string;
+  tags?: string[];
+  favourite!: boolean;
+  stars!: number;
+  imageURL!: string;
+  origins!: string[];
+  cookTime!: string;
+}
