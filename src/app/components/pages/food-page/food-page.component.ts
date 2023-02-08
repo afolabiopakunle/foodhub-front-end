d56951bfc1cf2dc62cb9b3e379f11589868ca746
Afolabi <opakunleafolabi@gmail.com>
2023-02-08 10:51:30 +0100

    add to cart implemented on food page

--- src/app/components/pages/food-page/food-page.component.ts
+++ src/app/components/pages/food-page/food-page.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Food } from '../../../shared/models/food';
 import { FoodService } from '../../../services/food.service';
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-food-page',
@@ -14,12 +15,19 @@ export class FoodPageComponent {
 
   constructor(private activatedRoute: ActivatedRoute,
               private foodService: FoodService,
+              private cartService: CartService,
+              private router: Router,
               ) {
   }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe(params => {
       this.food = this.foodService.getFoodById(params['id']);
-    })
+    });
+  }
+
+  addToCart() {
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
   }
 }
