830a6799b00a2c8f22c32596a913489d08860189
Afolabi <opakunleafolabi@gmail.com>
2023-02-09 09:21:32 +0100

    not-found implemented

--- src/app/components/partials/not-found/not-found.component.ts
+++ src/app/components/partials/not-found/not-found.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent {
+
+  @Input() visible = false;
+  @Input() notFoundMessage = 'Nothing found';
+  @Input() resetLink = '/';
+  @Input() resetLinkText = 'Reset';
 
 }
